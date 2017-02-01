2152aa7636e2c51874d7a4fcb2e595e89125930a
latke15 <nadav_baruch@hotmail.com>
2017-02-01 21:50:46 +0100

    nog wat layout gedaan

---
+++ Help me study/Info.swift
@@ -0,0 +1,17 @@
+//
+//  Info.swift
+//  Help me study
+//
+//  Created by Nadav Baruch on 01-02-17.
+//  Copyright © 2017 Nadav Baruch. All rights reserved.
+//
+
+import UIKit
+
+class Info: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "escher")!)
+    }
+}
